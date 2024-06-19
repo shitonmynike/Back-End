1ca6078f7950bf1ed94bcac08517d15bacbeaf2c
shitonmynike <jhonesja123@gmail.com>
2024-06-19 19:33:28 -0300

    teste

---
+++ petvet/back/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.listen(3000, function(){
+    console.log("começou");
+});
+
+//MVC
+// Model -> dados
+// VIew -> visual (front end)
+// Controler -> controlador -> ligamento
+
+
+
+
